1c26ea16b8f9b927adb9c2b32c68908803da408b
dodoangrybird <45337908+dodoangrybird@users.noreply.github.com>
2021-09-28 17:00:36 +0200

    Added ProgressCircularIndicator on CourseImage & Fixed GridView child widgets height and width

--- lib/CourseDetail.dart
+++ lib/CourseDetail.dart
@@ -3,44 +3,69 @@ import 'package:flutter/material.dart';
 class CourseDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      elevation: 0,
-      backgroundColor: Colors.transparent,
-      leading: IconButton(icon: Icon(Icons.arrow_back_ios,color: Colors.black,),onPressed: ()=>Navigator.pop(context)),
-      automaticallyImplyLeading: true,
-    ),
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(26.0),
-        child:  SingleChildScrollView(
-          child: Column(children: [
-              Container(width: 300,height: 300,decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.grey)),
-            SizedBox(height: 10),
-              Text("Course Name",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w800)),
-            SizedBox(height: 10),
-              Align(alignment:Alignment.centerLeft,child: const Text("About the course",style: TextStyle(fontSize:18,fontWeight: FontWeight.w600))),
-            SizedBox(height: 10),
-              const Text("bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla "),
-            SizedBox(height: 10),
-              Row(children: [Icon(Icons.video_call), Text("16 hours")]),
-            SizedBox(height: 10),
-             /* Row(children: [Icon(Icons.video_call), Text("16 hours")]),
-              Row(children: [Icon(Icons.video_call), Text("16 hours")]),*/
-              Align(alignment:Alignment.centerLeft,child: const Text("Curriculum",style: TextStyle(fontSize: 19,fontWeight: FontWeight.w600))),
-            SizedBox(height: 10),
-              ListView.builder(shrinkWrap:true,physics: NeverScrollableScrollPhysics(),itemBuilder: (context, index) => Card(
-                  elevation: 2,
-                    child: ListTile(title: Text("Introduction"),
-                        subtitle: Text("02:07 mins"),
-                        trailing: Icon(Icons.play_circle_fill)),
-                ),
-                    itemCount: 10),
-
-            ],),
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              ),
+              onPressed: () => Navigator.pop(context)),
+          automaticallyImplyLeading: true,
         ),
-
-      ),
-    ));
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(26.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                      width: 300,
+                      height: 300,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey)),
+                  SizedBox(height: 10),
+                  Text("Course Name",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
+                  SizedBox(height: 10),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text("About the course",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600))),
+                  SizedBox(height: 10),
+                  const Text(
+                      "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla "),
+                  SizedBox(height: 10),
+                  Row(children: [Icon(Icons.video_call), Text("16 hours")]),
+                  SizedBox(height: 10),
+                  /* Row(children: [Icon(Icons.video_call), Text("16 hours")]),
+              Row(children: [Icon(Icons.video_call), Text("16 hours")]),*/
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text("Curriculum",
+                          style: TextStyle(
+                              fontSize: 19, fontWeight: FontWeight.w600))),
+                  SizedBox(height: 10),
+                  ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) => Card(
+                            elevation: 2,
+                            child: ListTile(
+                                title: Text("Introduction"),
+                                subtitle: Text("02:07 mins"),
+                                trailing: Icon(Icons.play_circle_fill)),
+                          ),
+                      itemCount: 10),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kdeccourse/CourseDetail.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,7 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-
   final String title;
 
   @override
@@ -34,50 +35,111 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Stack(
-
-                    children: <Widget>[
-
-                      Container(height: MediaQuery.of(context).size.width/2.5,decoration: BoxDecoration(borderRadius: BorderRadius.only(bottomRight: Radius.circular(20),bottomLeft: Radius.circular(20)),color: Colors.blue),),
-                      Positioned(
-                        bottom: 20,
-                        right: 30,
-                        left:30,
-                        child: Container(
-                            child: TextField (
-
-                              decoration: InputDecoration(
-                                filled: true,
-                                fillColor: Colors.white,
-
-                                suffixIcon: Icon(Icons.search),
-                                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                                labelText: 'Search',
-
-                              ),
-                            ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      height: MediaQuery.of(context).size.width / 2.5,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(20)),
+                          color: Colors.blue),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      right: 30,
+                      left: 30,
+                      child: Container(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            suffixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            labelText: 'Search',
                           ),
+                        ),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.all(26.0),
-                    child: Align(alignment:Alignment.centerLeft,child: Text("Popular Courses",style: TextStyle(fontSize: 24,fontWeight: FontWeight.w600),)),
-                  ),
-                   GridView.builder(shrinkWrap:true,physics: NeverScrollableScrollPhysics(),itemBuilder: (context, index) => Column(children: [
-                          Container(width: 150,height: 150,decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: Colors.grey,),),
-                          Text("Course Name")
-                        ],),itemCount: 10,scrollDirection: Axis.vertical, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(26.0),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Popular Courses",
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600),
+                      )),
+                ),
+                GridView.builder(
 
-                ],
-              ),
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  padding: const EdgeInsets.all(8),
+                  itemBuilder: (context, index) => GestureDetector(
+                    onTap: ()=> Navigator.push(context, MaterialPageRoute(builder: (context) => CourseDetail())),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Container(
+                                    width: 300,
+                                    height: 500,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                              ),
+                              Positioned(
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                top: 0,
+                                child: CircularPercentIndicator(
+                                 radius: 45,
+                                  percent: 0.100,
+                                  center: new Text("10%",style: TextStyle(fontSize:10),),
+                                  progressColor: Colors.red,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text("Course Name"),
+                        SizedBox(height: 5,),
+                       /* Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: LinearPercentIndicator(
+                            lineHeight:10,
+                            percent: 0.100,
+                            center: new Text("10%",style: TextStyle(fontSize:10),),
+                            progressColor: Colors.red,
+                          ),
+                        ),*/
+                      ],
+                    ),
+                  ),
+                  itemCount: 10,
+                  scrollDirection: Axis.vertical,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    childAspectRatio: (1 / 1.2)),
+                ),
+              ],
             ),
           ),
-
+        ),
       ),
     );
   }
